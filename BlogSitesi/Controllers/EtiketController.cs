f2ae6c7e0b1364fcefe0670cf3b98fbab690c4aa
Emre-krgz <ramazan-emre93@hotmail.com>
2017-07-14 01:32:57 +0300

    Create Tags, Search, Categories
    RenderSection method created category, tag, search widget

---
+++ BlogSitesi/Controllers/EtiketController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSitesi.Controllers
+{
+    using Models;
+    public class EtiketController : Controller
+    {
+        BlogDbContext context = new BlogDbContext();
+        // GET: Etiket
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult EtiketWidger()
+        {
+            return PartialView(context.Etikets.ToList());
+        }
+    }
+}
---
+++ BlogSitesi/Controllers/KategoriController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSitesi.Controllers
+{
+    using Models;
+    public class KategoriController : Controller
+    {
+        BlogDbContext context = new BlogDbContext();
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult KategoriWidget()
+        {
+            return PartialView(context.Kategoris.ToList());
+        }
+    }
+}
