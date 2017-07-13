98e72927c043c9e76821a9a60f2564f131f4d28a
Emre-krgz <ramazan-emre93@hotmail.com>
2017-07-14 02:03:56 +0300

    Popular Post Widget Created
    For pictures, created settings.

--- BlogSitesi/Controllers/HomeController.cs
+++ BlogSitesi/Controllers/HomeController.cs
@@ -6,12 +6,20 @@ using System.Web.Mvc;
 
 namespace BlogSitesi.Controllers
 {
+    using Models;
     public class HomeController : Controller
     {
+        BlogDbContext context = new BlogDbContext();
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+
+        public PartialViewResult PopulerMakalelerWidget()
+        {
+            var model = context.Makales.OrderByDescending(x => x.EklenmeTarihi).Take(3).ToList();
+            return PartialView(model);
+    }
     }
 }
